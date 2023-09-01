5e07effe54930630915988d0ce2a3c6d89898027
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-09-01 18:50:09 -0300

    posição aleatória na tela

--- jogo-js.js
+++ jogo-js.js
@@ -1,18 +1,36 @@
-alert('teste')
-
 var altura = 0
 var largura = 0
 
-function ajustaTamanhoPalcoJogo(){
-    var altura = window.innerHeight
-    var largura = window.innerWidth
-    
-    console.log(altura, largura)
-    
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
 
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
 
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY //Se posição for menor que zero, ela recebe 0, se não, recebe o valor dela mesma
+    
+    console.log(posicaoX, posicaoY)
+    
+    //criar o elemento html
+    
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png' //incluindo imagem
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+    document.body.appendChild(mosquito) //adicionando um filho para o body
+    
+    
+    
+}
 
