116c0ed99de6011a33e3c026112f97ddb82b4701
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-09-01 18:17:41 -0300

    redimencionamento da tela do usuário

---
+++ jogo-js.js
@@ -0,0 +1,18 @@
+alert('teste')
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+    
+    console.log(altura, largura)
+    
+}
+
+ajustaTamanhoPalcoJogo()
+
+
+
+
